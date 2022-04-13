d1859e445d50ddf98d0aec514bc542c4c7fab058
g.bychkov <g.bychkov@grcc.tech>
2022-04-14 00:29:24 +0300

    DRAFT

--- script.js
+++
@@ -1,7 +0,0 @@
-function changePage (link) {
-    event.preventDefault()
-    document.querySelectorAll('.page').forEach(el => {
-        el.classList.add('d-none')
-    })
-    document.getElementById(link).classList.remove('d-none')
-}
